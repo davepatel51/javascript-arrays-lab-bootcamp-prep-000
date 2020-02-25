3471f53d2b34304abc799348d36726b1e228c9d5
Dave Patel <ddp2134@gmail.com>
2020-02-25 00:22:09 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,9 @@
-describe('Arrays', function() 
-{
+describe('Arrays', function() {
   describe('kittens', function() {
-    it('defines kittens as `var kittens = ["Milo","Otis", "Garfield"]`',  function() 
+    it('defines kittens as `var kittens = ['Milo','Otis', 'Garfield']`',  function();
   }
 }
+
 
   describe('destructivelyAppendKitten(name)', function() {
     it('appends a kitten to the end of the kittens array', function() {
